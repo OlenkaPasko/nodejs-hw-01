fab507d3c10f35bea2e3d360346c4e1cae93817a
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-08-25 15:32:32 +0300

    start

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,14 @@
-import { PATH_DB } from '../constants/contacts.js';
+//import { PATH_DB } from '../constants/contacts.js';
 
-const generateContacts = async (number) => {};
+import createFakeContact from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+  const items = [];
+  for (let i = 0; i < number; i += 1) {
+    const contactList = createFakeContact();
+    items.push(contactList);
+  }
+  console.log(items);
+};
 
 generateContacts(5);
