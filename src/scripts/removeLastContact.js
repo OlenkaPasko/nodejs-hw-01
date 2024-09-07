e349d84b797b10a4b349e81a515f7b5b5dd480f5
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-09-07 16:25:59 +0300

    removeLastContact

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,10 @@
-import { PATH_DB } from '../constants/contacts.js';
+import getAllContacts from './getAllContacts.js';//отримати весь список
+import updateContacts from './updateContacts.js';//перезаписати джейсон
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+    const contactList = await getAllContacts();
+    contactList.pop();
+    await updateContacts(contactList);
+};
 
 removeLastContact();
