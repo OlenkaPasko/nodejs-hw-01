a1b44a9ebbc174f7d6350bcb3fac9ab1794a39b2
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-09-08 14:07:56 +0300

    getAllContacts

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -5,5 +5,7 @@ const getAllContacts = async () => {
     const data = await fs.readFile(PATH_DB, "utf-8");//читаємо дані
      return JSON.parse(data);//повертаємо дані пропушені через джейсон.парс
 };
+console.log(await getAllContacts());
 
 export default getAllContacts;
+
